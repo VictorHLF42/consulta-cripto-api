50056c234b51929cd008223879f7c0fb547a1ed7
victor.souza <fuhielb550@gmail.com>
2025-08-27 11:08:43 -0300

    feat: Adiciona o DbContext e instala pacotes para persistência de dados

--- Domain/CryptoCurrency.cs
+++ Domain/CryptoCurrency.cs
@@ -1,20 +1,13 @@
 ﻿using System;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-
 namespace Domain
 {
     public class CryptoCurrency
     {
-        [Key]
         public int Id { get; set; }
-        [Column(TypeName = "TEXT")]
         public string? Symbol { get; set; }
-        [Column(TypeName = "REAL")]
         public decimal Price { get; set; }
-        [Column(TypeName = "DATETIME")]
-        public DateTime CreatedAt{ get; set; }
-
-
+        public DateTime CreatedAt { get; set; }
     }
 }
---
+++ Infra/Data/CryptoDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infra.Data
+{
+    public class CryptoDbContext : DbContext
+    {
+        public CryptoDbContext(DbContextOptions<CryptoDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<CryptoCurrency> Cryptocurrencies { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<CryptoCurrency>(entity =>
+            {
+                entity.ToTable("Cryptocurrency");
+                entity.HasKey(e => e.Id);
+                entity.Property(e => e.Symbol).HasColumnType("TEXT").IsRequired();
+                entity.Property(e => e.Price).HasColumnType("REAL").IsRequired();
+                entity.Property(e => e.CreatedAt).HasColumnType("DATETIME").IsRequired();
+            });
+        }
+    }
+}
