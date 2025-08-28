0ff0a3a87baea4893726f5225ffeff7e972cf69c
victor.souza <fuhielb550@gmail.com>
2025-08-28 15:58:37 -0300

    feat: Implementa o repositório de dados na camada Infra

---
+++ Infra/Data/CryptoRepository.cs
@@ -0,0 +1,23 @@
+﻿using Domain.Interfaces;
+using Domain;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+
+namespace Infra.Data
+{
+    public class CryptoRepository : ICryptoRepository
+    {
+        private readonly CryptoDbContext _context;
+
+        public CryptoRepository(CryptoDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<CryptoCurrency> GetBySymbolAsync(string symbol)
+        {
+            return await _context.Cryptocurrencies
+                                .FirstOrDefaultAsync(c => c.Symbol == symbol);
+        }
+    }
+}
