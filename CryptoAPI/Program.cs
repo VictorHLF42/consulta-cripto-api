31b70b891c7951ac6d692584c305f0699263c1fb
victor.souza <fuhielb550@gmail.com>
2025-08-29 16:07:14 -0300

    feat: Adiciona configuracao de injecao de dependencia e pacote FluentResults

--- CryptoAPI/Program.cs
+++ CryptoAPI/Program.cs
@@ -1,6 +1,19 @@
+using Infra.Data;
+using Domain.Interfaces;
+using Application.ExternalServices;
+using Application.Services;
+using Microsoft.EntityFrameworkCore;
+using System.Net.Http; 
+using FluentResults; 
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+
+builder.Services.AddDbContext<CryptoDbContext>(options => options.UseSqlite("Data Source=crypto.db"));
+builder.Services.AddHttpClient<CoinMarketCapClient>();
+builder.Services.AddScoped<ICryptoRepository, CryptoRepository>();
+builder.Services.AddScoped<CryptoPriceService>();
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -9,7 +22,7 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
