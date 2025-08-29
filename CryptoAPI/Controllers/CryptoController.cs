a0cfbab9dac1eba57b68c5b07d6cbb0cb2c9c036
victor.souza <fuhielb550@gmail.com>
2025-08-29 16:18:10 -0300

    feat: Adiciona controller e finaliza a API de consulta de criptomoeda

---
+++ CryptoAPI/Controllers/CryptoController.cs
@@ -0,0 +1,33 @@
+﻿using Application.Services;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace CryptoAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CryptoController : ControllerBase
+    {
+        private readonly CryptoPriceService _cryptoPriceService;
+
+        public CryptoController(CryptoPriceService cryptoPriceService)
+        {
+            _cryptoPriceService = cryptoPriceService;
+        }
+
+        [HttpGet("{symbol}")]
+        public async Task<ActionResult<decimal?>> GetCryptoPrice(string symbol)
+        {
+            var price = await _cryptoPriceService.GetPriceBySymbolAsync(symbol);
+
+            if (price == null)
+            {
+                // Retorna 404 Not Found se a moeda não for encontrada.
+                return NotFound();
+            }
+
+            // Retorna 200 OK com o preço.
+            return Ok(price);
+        }
+    }
+}
