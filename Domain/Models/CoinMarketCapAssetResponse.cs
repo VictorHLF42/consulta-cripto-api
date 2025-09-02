52ab0e7338b2eff2b9492650bb6c0188e9702883
victor.souza <fuhielb550@gmail.com>
2025-09-02 17:01:10 -0300

    refactor: Refatora entidade CryptoCurrency e adiciona models para API externa

---
+++ Domain/Models/CoinMarketCapAssetResponse.cs
@@ -0,0 +1,20 @@
+﻿using System.Text.Json.Serialization;
+
+public class CoinMarketCapAssetResponse
+{
+    [JsonPropertyName("data")]
+    public Dictionary<string, Asset[]> Data { get; set; }
+
+    public class Asset
+    {
+        
+        [JsonPropertyName("quote")]
+        public Dictionary<string, AssetInformation> Quote { get; set; }
+    }
+
+    public class AssetInformation
+    {
+        [JsonPropertyName("price")]
+        public decimal? Price { get; set; }
+    }
+}
