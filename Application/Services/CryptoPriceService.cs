62cc2af5ade8e1d7084ddd081ac334721826d515
victor.souza <fuhielb550@gmail.com>
2025-10-06 14:50:14 -0300

    feat: Atualiza namespace e adiciona CryptoPriceService

--- Application/Services/CryptoPriceService.cs
+++ Application/Services/CryptoPriceService.cs
@@ -7,8 +7,8 @@ using System;
 using System.Collections.Generic; 
 using System.Threading.Tasks;
 
-namespace Application.Services
-{
+namespace Application.Services;
+
     public class CryptoPriceService
     {
         private readonly ICryptoRepository _cryptoRepository;
